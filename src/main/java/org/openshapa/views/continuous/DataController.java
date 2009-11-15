65375c768b125d963929343d22f663d94eff88ca
Clinton <clinton.freeman@nicta.com.au>
2009-11-15 14:23:21 +1000

    Added interface for DataController.

---
+++ src/main/java/org/openshapa/views/continuous/DataController.java
@@ -0,0 +1,22 @@
+package org.openshapa.views.continuous;
+
+/**
+ * Creators of plugins DO NOT NEED TO IMPLEMENT this class - the method
+ * 'setParentController' of DataViewer is used to notify each viewer of its
+ * parent controller. When an implemeting viewer is closed, it should notify
+ * the parent controller that it no longer requires commands by calling
+ * shutdown and supplying itself as the argument. i.e.
+ *
+ * parentController.shutdown(this);
+ *
+ * It would be nice if we could use a weak reference instead (see bug:755)
+ */
+public interface DataController {
+    /**
+     * Remove the specifed viewer form the controller.
+     *
+     * @param viewer The viewer to shutdown.
+     * @return True if the controller contained this viewer.
+     */
+    boolean shutdown(final DataViewer viewer);
+}

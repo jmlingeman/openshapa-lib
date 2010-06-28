4a734d10e2dcac38ac039d53ca96189b6ff77696
Nicholas <nicholas.allen@nicta.com.au>
2010-06-28 12:42:02 +1000

    BugzID:1935 - Missed out a class

---
+++ src/main/java/org/openshapa/views/continuous/ViewerStateListener.java
@@ -0,0 +1,14 @@
+package org.openshapa.views.continuous;
+
+
+/**
+ * Viewer listener interface.
+ */
+public interface ViewerStateListener {
+
+    /**
+     * Notify listener of a change made to the project.
+     */
+    void notifyStateChanged();
+
+}

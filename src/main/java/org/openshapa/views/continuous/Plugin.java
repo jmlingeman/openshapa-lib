c9a83c01bb0a2969f76701e16263940af722748b
Douglas Teoh <douglas.teoh@nicta.com.au>
2010-08-10 13:26:30 +1000

    BugzID:2108 - Added plugin classifier.

--- src/main/java/org/openshapa/views/continuous/Plugin.java
+++ src/main/java/org/openshapa/views/continuous/Plugin.java
@@ -21,10 +21,31 @@ public interface Plugin {
     FileFilter getFileFilter();
 
     /**
-     * @return The icon for representing this plugin. This can return {@code null} if
+     * @return The icon for representing this plugin. This can return null if
      *         this plugin has no icon representing its type.
      */
     ImageIcon getTypeIcon();
+
+    /**
+     *<p>
+     * A classifier string is a string representing the class of your plugin as
+     * part of some namespace. An example of such a string could be
+     * "companyfoo.video", which a person could interpret as "a plugin by
+     * company foo which handles video". OpenSHAPA will not attempt to
+     * interpret the string.
+     *</p>
+     *<p>
+     * This classifier is used to group plugins with the same or similar
+     * capabilities together. This grouping is used to select a backup or
+     * alternative plugin (with the same classifier and file type handling) in
+     * the case where the plugin used to visualize some data is not installed.
+     *</p>
+     *<p>
+     * The classifier string cannot be null or the empty string.
+     *</p>
+     * @return Classifier string.
+     */
+    String getClassifier();
 
     /**
      * @return True if the first custom action is supported. False otherwise.
